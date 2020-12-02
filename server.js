0a7d03c361067b13fa7388be7f22f910af410463
losrobbos <losrobbos@web.de>
2020-12-02 12:17:01 +0100

    Finished feature chat messages

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 const express = require("express")
 const app = express()
+const socketIo = require("socket.io")
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log("API server started up", PORT))
+// save instance of the started server
+const server = app.listen(PORT, () => console.log("API server started up", PORT))
 
 // setup socket server here...
+const io = socketIo(server, {
+  cors: {
+    origin: "*" // allow all origins connecting
+  }
+}) // io = input output
+
+app.get("/", () => {
+  res.json({ message: "Hello" })
+})
+
+// start listening for incoming socket connections
+  // runs once for every connecting client
+io.on("connection", (socket) => {
+
+  // socket = direct channel / line between frontend & backend
+  console.log("Someone connected...")
+
+  // emit event (message) to frontend! 
+  // socket.emit('message', { message: "Welcome my friend" })
+
+  // listen for incoming mesages
+  socket.on('message', (chatMsg) => {
+    console.log(chatMsg)
+    socket.broadcast.emit('message', chatMsg) // here we omit the sender
+  })
+
+})
+
+
+
+
